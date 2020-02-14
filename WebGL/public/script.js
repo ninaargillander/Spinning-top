26261a555957a7eb519f85c45c6bab6331a7dcea
Loual201 <louise.almer@hotmail.com>
2020-02-14 15:59:25 +0100

    Den snurrar lite nu

--- WebGL/public/script.js
+++ WebGL/public/script.js
@@ -1,5 +1,6 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var object;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -9,9 +10,10 @@ var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 
 camera.position.z = 20;
+camera.position.y = 5;
 
 var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
-keyLight.position.set(-100, 0, 100);
+keyLight.position.set(-100, 100, 100);
 
 var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
 fillLight.position.set(100, 0, 100);
@@ -28,7 +30,9 @@ objLoader.setPath('/assets/');
 objLoader.load('Test_snurra.obj', function(object) {
 	object.position.y -= 5;
 	scene.add(object);
+	setInterval(function(){ object.rotateY(Math.PI/6); }, 100);
 });
+
 
 var animate = function () {
 	requestAnimationFrame( animate );
