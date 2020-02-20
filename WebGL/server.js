5f7e8cbf8a890e2bfc2c8f957b1991b077f26bb4
Nina Argillander <nina@argillander.se>
2020-02-20 14:57:03 +0100

    Testar

--- WebGL/server.js
+++ WebGL/server.js
@@ -1,14 +1,15 @@
 var express = require('express'),
-    path = require('path'),
-    app = express();
+  path = require('path'),
+  app = express();
 
 app.set('port', (process.env.PORT || 8080));
 
 app.use(express.static('public'));
 
-app.listen(app.get('port'), function(err) {
+app.listen(app.get('port'), function (err) {
   if (err) {
     console.log(err);
   } else {
-    console.log('Running on port: ' + app.get('port')); }
+    console.log('Running on port: ' + app.get('port'));
+  }
 });
