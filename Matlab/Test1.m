c0d057c305265e545c99d6d55e8496dbbdadc701
Matilda Eriksson <erikssonmatilda@live.se>
2020-02-25 18:59:54 +0100

    Testade lite med att lägga till en friktionskraft, fick inte riktigt ut något vettigt dock

--- Matlab/Test1.m
+++ Matlab/Test1.m
@@ -1,15 +1,22 @@
 F = 1;
-m = 0.5;
+m = 0.013;
 r = 0.02;
-h = 0.04;
+h = 0.07;
 g = 9.82;
 delta_t = 1;
-I3 = (3*m*r*r)/10;
 l = 3*h/4;
-I1 = m * ((3/20)*r*r + (3/80)*h*h);
+my = 0.48;
+Friction = -1 * my * m * g; % Frictionskraften, negativ normalkraft gånger friktionkoefficient
+r_contact = 0.001;  % Radien på kontaktpunkten?
 
-psi_dot = F*r*delta_t/I3;
-phi_dot = m*g*l/(psi_dot*I1);
+I1 = m * ((3/20)*r*r + (3/80)*h*h);
+I3 = (3*m*r*r)/10;
+
+psi_dot_dot = Friction * l / I3;
+psi_dot(1) = F*r*delta_t/I3;
+phi_dot(1) = m*g*l/(psi_dot(1)*I1);
+
+psi_dot_limit = 4*I1*m*g*cos(pi/12) / (I3*I3);
 
 H=0.001; % step's size
 N=1000; % number of steps
@@ -17,21 +24,37 @@ psi(1) = 0;
 phi(1) = 0;
 
 for n=1:N
-    psi(n+1) = Euler( psi(n), psi_dot, H);
-    phi(n+1) = Euler(phi(n), phi_dot, H);
+    psi_dot(n+1) = Euler(psi_dot(n), psi_dot_dot, H);
+    phi_dot(n+1) = m*g*l/(psi_dot(n+1)*I1);
+        
+    if psi_dot(n+1) * psi_dot(n+1) < psi_dot_limit
+       psi_dot(n+1) = 0;
+       phi_dot(n+1) = 0;
+    end
+    
+    psi(n+1) = Euler(psi(n), psi_dot(n), H);
+    phi(n+1) = Euler(phi(n), phi_dot(n), H);
 end
 
+% Detta resultat stämmer verkligen inte, men kanske är lite på väg i alla
+% fall. Graferna blir i alla fall lite spännande!
 t = 1:H:H*N+1;
-%plot(t,mod(psi,2*pi),'r')
-%figure;
-%plot(t,mod(phi,2*pi),'r')
+plot(t,mod(psi, 2*pi),'r');
 figure;
+plot(t,mod(phi,2*pi),'r');
+figure;
+plot(t,psi_dot,'r');
+figure;
+plot(t,phi_dot,'r');
 
+% Märkte när jag körde denna nedan att mittensnurren ändrar riktning efter en
+% stund och börjar gå åt andra hållet. Vet ej vad det beror på. Den andra
+% rör sig jättesakta, känns inte heller så bra.
 
-for n =1:N
-    plot(r*cos(psi(n)), r*sin(psi(n)), '*');
-    axis([-1 1 -1 1]);
-    hold on;
-    plot(cos(phi(n)), sin(phi(n)), '*');
-    drawnow();
-end
+% for n =1:N
+%     plot(r*cos(psi(n)), r*sin(psi(n)), 'x');
+%     axis([-1 1 -1 1]);
+%     hold on;
+%     plot(cos(phi(n)), sin(phi(n)), '*');
+%     drawnow();
+% end
