8ef701404efaff0eafd3bf4e5c80513dae78c1cb
Nina Argillander <nina@argillander.se>
2020-02-28 16:15:50 +0100

    Skuggor funkar nu

--- WebGL/public/script.js
+++ WebGL/public/script.js
@@ -12,54 +12,34 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-//Skugga
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFShadowMap;
-
-
 camera.position.z = 20;
 camera.position.y = 5; //fem för snurran
 
-var box = new THREE.BoxGeometry(3, 3, 3);
-var boxMaterial = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(box, boxMaterial);
-cube.position.set(0, 3, 0)
-
-cube.castShadow = true;
-cube.receiveShadow = true;
-
-scene.add(cube);
-
+//Ljuskällor
+var backLight = new THREE.HemisphereLight(new THREE.Color('hsl(34, 45%, 80%)'), new THREE.Color('hsl(30, 38%, 50%)'), 1);
+scene.add(backLight)
 
 var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(210, 100%, 75%)'), 0.75);
-fillLight.position.set(0, 10, 0);
-//fillLight.target.position.set(0, 0, 0);
+fillLight.position.set(100, 100, 0);
 
-var backLight = new THREE.HemisphereLight(new THREE.Color('hsl(34, 45%, 80%)'), new THREE.Color('hsl(30, 38%, 50%)'), 1);
+//Skugga
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 fillLight.castShadow = true;
-//skugga
-fillLight.castShadow = true;
-fillLight.shadowDarkness = 0.5;
+fillLight.shadowDarkness = 1;
 
-fillLight.shadowCameraNear = 15;
-fillLight.shadowCameraFar = -10;
+fillLight.shadowCameraNear = 0.5;
+fillLight.shadowCameraFar = 5000;
 
-fillLight.shadow.bias = 0.0001;
-
-fillLight.shadow.mapSize.width = 1024;
-fillLight.shadow.mapSize.height = 1024;
-
-/*
-fillLight.shadowCameraRight = 5;
-fillLight.shadowCameraLeft = -5;
-fillLight.shadowCameraTop = 5;
-fillLight.shadowCameraBottom = -5;
-
-*/
+fillLight.shadowCameraRight = 50;
+fillLight.shadowCameraLeft = -50;
+fillLight.shadowCameraTop = 50;
+fillLight.shadowCameraBottom = -50;
 
 scene.add(fillLight);
-scene.add(backLight);
+
+//*******************Miljö*****************************//
 
 var textureLoader = new THREE.TextureLoader();
 
@@ -78,7 +58,6 @@ floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
 floorTexture.repeat.set(8, 8);
 
 skyTexture.wrapS = skyTexture.wrapT = THREE.RepeatWrapping;
-//skyTexture.repeat.set(5, 5);
 
 //Skapa mesh av planen med textur
 var sky = new THREE.Mesh(skyPlane, skyMaterial);
@@ -86,23 +65,17 @@ sky.position.set(0, 0, -25)
 
 var floor = new THREE.Mesh(floorPlane, floorMaterial);
 floor.rotateX(-Math.PI / 2);
+floor.receiveShadow = true;
 
-
+//Lägg allt till scenen
 scene.add(mainContainer);
 mainContainer.add(container);
 scene.add(sky)
 scene.add(floor);
 
-//floor.recieveShadow = true;
+//***********************************************************//
 
-floor.receiveShadow = true;
 
-//ta bort 
-scene.add(new THREE.CameraHelper(camera))
-var helper = new THREE.CameraHelper(fillLight.shadow.camera);
-scene.add(helper);
-
-console.log(fillLight);
 //*******************Ekvationer*****************************//
 
 //Snurrans egenskaper
@@ -140,13 +113,16 @@ for (var i = 0; i < howManyPsi; ++i) {
 	psi[i + 1] = euler(psi[i], psi_dot, stepLength);
 	phi[i + 1] = euler(phi[i], phi_dot, stepLength);
 }
+
 //*****************************************************************//
 
-/*
+
+//Läs in snurran med textur
 var mtlLoader = new THREE.MTLLoader();
 mtlLoader.setTexturePath('/assets/');
 mtlLoader.setPath('/assets/');
 mtlLoader.load('spintop.mtl', function (materials) {
+
 	materials.preload();
 
 	var objLoader = new THREE.OBJLoader();
@@ -155,17 +131,17 @@ mtlLoader.load('spintop.mtl', function (materials) {
 	objLoader.load('spintop.obj', function (object) {
 
 		object.position.x = 0;
-		object.position.y = 2;
+		object.position.y = 0.45;
 		object.position.z = 0;
 
-		object.castShadow = true;
+		object.children["0"].castShadow = true;
 		object.recieveShadow = false;
 
 		container.add(object);
 
 	});
 })
-*/
+
 var k = 0;
 
 var animate = function () {
@@ -177,7 +153,6 @@ var animate = function () {
 	thetaRotation(container, 0.4);
 
 	if (k == howManyPsi) k = 0;
-	//console.log('K: ' + k)
 	k++;
 
 	renderer.render(scene, camera);
