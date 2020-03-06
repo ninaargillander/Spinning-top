bf3ef86e5888f73b96ff7f6fe51411bac227e28c
Nina Argillander <nina@argillander.se>
2020-03-06 13:35:22 +0100

    ..

--- WebGL/public/psiRotation.js
+++ WebGL/public/psiRotation.js
@@ -1,20 +1,7 @@
 export function psiRotation(object, angle) {
-<<<<<<< HEAD
-=======
-   
->>>>>>> 66d91b435c90bc7456460ac153a30f63e651410c
     //Bestämmer axlarnas rotationsordning till först rotation runt z, sen x, sen y
     object.children["0"].children["0"].rotation.order = "ZXY"
 
     //Sätter rotation runt egen axel.
     object.children["0"].children["0"].rotation.y = angle;
-<<<<<<< HEAD
 };
-
-
-
-=======
-    //console.log(object);
-
-};
->>>>>>> 66d91b435c90bc7456460ac153a30f63e651410c
--- WebGL/public/thetaRotation.js
+++ WebGL/public/thetaRotation.js
@@ -1,9 +1,13 @@
 export function thetaRotation(object, angle) {
-   
+
     //Sätter ordningen av vilka rotationer utförs först
     object.rotation.order = "YXZ";
 
     //Roterar containern, så objektet roteras runt världens x- och z-axel
-    object.children["0"].children["0"].rotation.x = angle;
     object.children["0"].children["0"].rotation.z = angle;
+    object.children["0"].children["0"].rotation.x = angle;
+
 };
+
+
+
